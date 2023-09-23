a626ee336e0edfd154f22d364bbb62e8fdce8fe0
tolujohnson24 <tolujohnson@krownpay.com>
2023-09-23 12:43:13 +0100

    update email template

--- model/NewCompany.js
+++ model/NewCompany.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const NewCompanySchema = new mongoose.Schema({
     companyName: { type: String, required: true },
-    companyAddress: { type: String, required: true },
+    companyAddress: { type: String },
     generalSettings: {
         type: Map,
         of: mongoose.Schema.Types.Mixed,
