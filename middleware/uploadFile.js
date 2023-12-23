b3fd8af3c859810184c8543b8e01140e6b016400
tolujohnson24 <tolujohnson@krownpay.com>
2023-12-23 10:55:55 +0100

    fixed update employee

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -10,12 +10,15 @@ const imageUploader = async (req, res, next) => {
   try {
 
     console.log(req.file)
-    const b64 = Buffer.from(req.file.buffer).toString("base64");
-    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
-    const cldRes = await handleUpload(dataURI);
-
-    console.log({cldRes})
-    req.body.image = cldRes.secure_url;
+    if(req.file){
+      const b64 = Buffer.from(req.file.buffer).toString("base64");
+      let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+      const cldRes = await handleUpload(dataURI);
+  
+      console.log({cldRes})
+      req.body.image = cldRes.secure_url;
+    }
+   
     next()
     // res.json(cldRes);
   } catch (error) {
