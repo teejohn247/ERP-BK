2d71b3b6e18dc6bb2a7242eb7c84abc96914af83
tolujohnson24 <47560491+teejohn247@users.noreply.github.com>
2024-06-24 10:41:40 +0100

    fixed leave application

--- controller/Employers/fetchEmployees.js
+++ controller/Employers/fetchEmployees.js
@@ -9,6 +9,7 @@ import Roles from '../../model/Roles';
 import utils from '../../config/utils';
 
 import { emailTemp } from '../../emailTemplate';
+import Company from '../../model/Company';
 
 
 const sgMail = require('@sendgrid/mail')
@@ -28,33 +29,95 @@ const fetchEmployees = async (req, res) => {
 
         const { page, limit } = req.query;
 
-        const employee = await Employee.find({companyId: req.payload.id}).sort({_id: -1})
-        .limit(limit * 1)
-        .skip((page - 1) * limit)
-        .exec();
+        const employee = await Employee.findOne({_id: req.payload.id})
+
+        const company = await Company.findOne({_id: req.payload.id})
+
+        console.log({employee});
+        console.log({company});
+
+
+        if(employee){
+            const emp = await Employee.find({companyId: employee.companyId}).sort({_id: -1})
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .exec();
+            const employeeTable = await EmployeeTable.find()
+
+            console.log({emp})
+
+            const count = await Employee.find({companyId: employee.companyId}).countDocuments()
+    
+            if(!employee){
+                res.status(404).json({
+                    status:404,
+                    success: false,
+                    error:'No employee Found'
+                })
+                return
+            }else{
+                res.status(200).json({
+                    status: 200,
+                    success: true,
+                    employeeTable,
+                    data: emp,
+                    totalPages: Math.ceil(count / limit),
+                    currentPage: page
+                })
+            }
+        } else if(company)
+            {
+                const emp = await Employee.find().sort({_id: -1})
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .exec();
+                const employeeTable = await EmployeeTable.find()
+
+                const count = await Employee.find().countDocuments()
+        
+                if(!employee){
+                    res.status(404).json({
+                        status:404,
+                        success: false,
+                        error:'No employee Found'
+                    })
+                    return
+                }else{
+                    res.status(200).json({
+                        status: 200,
+                        success: true,
+                        employeeTable,
+                        data: employee,
+                        totalPages: Math.ceil(count / limit),
+                        currentPage: page
+                    })
+                }
+            } else {
+                const employeeTable = await EmployeeTable.find()
+
+                const count = await Employee.find().countDocuments()
+        
+                if(!employee){
+                    res.status(404).json({
+                        status:404,
+                        success: false,
+                        error:'No employee Found'
+                    })
+                    return
+                }else{
+                    res.status(200).json({
+                        status: 200,
+                        success: true,
+                        employeeTable,
+                        data: employee,
+                        totalPages: Math.ceil(count / limit),
+                        currentPage: page
+                    })
+                }
+            }
 
         
-        const employeeTable = await EmployeeTable.find()
-
-        const count = await Employee.find().countDocuments()
-
-        if(!employee){
-            res.status(404).json({
-                status:404,
-                success: false,
-                error:'No employee Found'
-            })
-            return
-        }else{
-            res.status(200).json({
-                status: 200,
-                success: true,
-                employeeTable,
-                data: employee,
-                totalPages: Math.ceil(count / limit),
-                currentPage: page
-            })
-        }
+      
 
     } catch (error) {
         res.status(500).json({
